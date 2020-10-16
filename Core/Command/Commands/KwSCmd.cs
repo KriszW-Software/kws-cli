f73c8b9fcc2f987a2ed3d10843f7dab1de64486b
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-10-16 09:28:39 +0200

    Root command added

---
+++ Core/Command/Commands/KwSCmd.cs
@@ -0,0 +1,13 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commands
+{
+    [Command(Name = "kwscli", OptionsComparison = StringComparison.InvariantCultureIgnoreCase)]
+    [VersionOptionFromMember("--version", MemberName = nameof(GetVersion))]
+    public class KwSCmd
+    {
+    }
+}
