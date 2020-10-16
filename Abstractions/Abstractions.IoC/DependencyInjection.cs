914cfaa3b98fa514ed7ca9af41b685fd4636c968
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-10-16 09:28:32 +0200

    Dependency Injection class added

---
+++ Abstractions/Abstractions.IoC/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Abstractions.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddCore(this IServiceCollection collection)
+        {
+            return collection;
+        }
+    }
+}
